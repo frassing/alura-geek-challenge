2f0d03aea47fee80c915823ef7d71fcf4fd56d77
frassing <raquel.f.roessing@hotmail.com>
2024-11-21 18:44:49 -0400

    add: api conection and controllers to fetch and add products

---
+++ assets/js/services/conectApi.js
@@ -0,0 +1,45 @@
+const BASE_URL = "http://localhost:3000/products";
+
+async function getProducts() {
+	try {
+		const response = await fetch(BASE_URL);
+		if (!response.ok) {
+			throw new Error("Não foi possível carregar produtos.");
+		}
+		const data = await response.json();
+		return data;
+
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function addProduct(name, price, image) {
+	try {
+		const response = await fetch(BASE_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				name: name,
+				price: price,
+				image: image
+			})
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			return data;
+		} else {
+			throw new Error("Não foi possível adicionar o produto.");
+		}
+	} catch (error) {
+		throw error;
+	}
+}
+
+export const conectApi = {
+	getProducts,
+	addProduct
+}
